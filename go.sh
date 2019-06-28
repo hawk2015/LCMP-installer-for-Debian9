d3e2eeb653ece071097958434b4ac0beeb26cc4f
MyMac <MyMac@MyMacBook.lan>
2019-06-28 14:47:44 +0800

    1

---
+++ go.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Caddy Web Server Installer script
+#
+
+
+if [[ -e "/etc/init.d/caddy" ]]; then
+	echo ""
+	echo "  Removing old Caddy script"
+	rm -f /etc/init.d/caddy
+fi
+echo ""
+echo "  Setting up Caddy"
+cd /tmp
+wget -q https://raw.githubusercontent.com/hawk2015/LCMP-installer-for-Debian9/master/caddy?token=AMHOMUDOINWHBGPRLTWDMBS5CW3NS
+chmod +x caddy
+mv caddy /etc/init.d
+echo "  Done. run 'caddy' to use Caddy"
+echo ""
+exit;
