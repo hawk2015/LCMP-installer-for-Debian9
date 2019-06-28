e1e84214e4095bccad255a6ca38ebf24f356a2de
MyMac <MyMac@MyMacBook.lan>
2019-06-28 15:22:30 +0800

    modify

--- go.sh
+++ go.sh
@@ -12,8 +12,8 @@ fi
 echo ""
 echo "  Setting up Caddy"
 cd /tmp
-wget -q https://raw.githubusercontent.com/hawk2015/LCMP-installer-for-Debian9/master/caddy?token=AMHOMUDOINWHBGPRLTWDMBS5CW3NS
-chmod +x caddy
+wget -q https://raw.githubusercontent.com/hawk2015/LCMP-installer-for-Debian9/master/caddy;
+chmod +x caddy;
 mv caddy /etc/init.d
 echo "  Done. run 'caddy' to use Caddy"
 echo ""
