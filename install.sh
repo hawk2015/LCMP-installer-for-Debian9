507a4472da8a2c35e9312aab24ef4030b6a6869b
MyMac <MyMac@MyMacBook.lan>
2019-06-29 22:50:31 +0800

    1

--- install.sh
+++ install.sh
@@ -26,57 +26,6 @@ about () {
 	echo "  ========================================================= "
 	echo ""
 }
-#check Caddy Installer new script
-if [[ -e "/tmp/caddy" ]]; then
-	echo ""
-	echo "  Removing old isntall script"
-	rm -f /tmp/caddy
-fi
-echo ""
-cd /tmp
-wget -q https://raw.githubusercontent.com/hawk2015/LCMP-installer-for-Debian9/master/caddy && chmod +x caddy
-mv caddy /tmp &>/dev/null
-echo ""
-
-input=
-until
- echo "----------LCMP Control Manu-----------"
- echo "Please enter your choise:(1-9)"
- echo "1. $($blue)Install$($normal) Caddy Web Server"
- echo "2. $($blue)Install$($normal) Caddy with plugins：http.cache,http.cors,http.expires,http.filter,http.git,tls.dns.cloudflare"
- echo "3. $($blue)Start$($normal) Caddy Web Server"
- echo "4. $($blue)Stop$($normal) Caddy Web Server"
- echo "5. $($blue)Check $($normal) Status of Caddy Web Server"
- echo "6. $($blue)Backup$($normal) The Caddy Config Files"
- echo "7. $($blue)Restore$($normal) The Caddy Config Files"
- echo "8. $($red)Delete$($normal) Caddy Web Server"
- echo "9. $($blue)Install$($normal) PHP7.3"
- echo "10. $($blue)Install$($normal) MariaDB (branch of MySQL)"
- echo "11. $($blue)Install$($normal) Wordpress"
- echo "12. Exit Menu"
- echo "--------------------------------------"
-
- read -r -p "  Input Number: " inputnum
- test $inputnum -eq 12
- do
-
-  case $inputnum in
- 1) bash /tmp/caddy install;;
- 2) bash /tmp/caddy install http.cache,http.cors,http.expires,http.filter,http.git,tls.dns.cloudflare;;
- 3) bash /tmp/caddy start;;
- 4) bash /tmp/caddy stop;;
- 5) bash /tmp/caddy status;;
- 6) bash /tmp/caddy backup;;
- 7) bash /tmp/caddy restore;;
- 8) bash /tmp/caddy delete;;
- 9) installphp;;
- 10) installsql;;
- 11) installwp;;
- *) echo "Sorry, Wrong number. try again, please!";;
-   esac
-
- done
-exit;
 
 #install php
 installphp () {
@@ -121,3 +70,57 @@ installwp() {
 	chown $caddyuser:$caddyuser $caddywww/wordpress
 	echo " Wordpress installation was[$($blue)DONE$($normal)]"
 }
+
+#check Caddy Installer new script
+if [[ -e "/tmp/caddy" ]]; then
+	echo ""
+	echo "  Removing old isntall script"
+	rm -f /tmp/caddy
+fi
+echo ""
+cd /tmp
+wget -q https://raw.githubusercontent.com/hawk2015/LCMP-installer-for-Debian9/master/caddy && chmod +x caddy
+mv caddy /tmp &>/dev/null
+echo ""
+
+input=
+until
+ echo "----------LCMP Control Manu-----------"
+ echo "Please enter your choise:(1-9)"
+ echo "1. $($yellow)Install$($normal) Caddy Web Server"
+ echo "2. $($yellow)Install$($normal) Caddy with plugins：http.cache,http.cors,http.expires,http.filter,http.git,tls.dns.cloudflare"
+ echo "3. $($blue)Start$($normal) Caddy Web Server"
+ echo "4. $($blue)Stop$($normal) Caddy Web Server"
+ echo "5. $($blue)Check $($normal) Status of Caddy Web Server"
+ echo "6. $($blue)Backup$($normal) The Caddy Config Files"
+ echo "7. $($blue)Restore$($normal) The Caddy Config Files"
+ echo "8. $($red)Delete$($normal) Caddy Web Server"
+ echo "9. $($blue)Install$($normal) PHP7.3"
+ echo "10. $($yellow)Install$($normal) MariaDB (branch of MySQL)"
+ echo "11. $($yellow)Install$($normal) Wordpress"
+ echo "12. about installer"
+ echo "13. Exit Menu"
+ echo "--------------------------------------"
+
+ read -r -p "  Input Number: " inputnum
+ test $inputnum -eq 13
+ do
+
+  case $inputnum in
+ 1) bash /tmp/caddy install;;
+ 2) bash /tmp/caddy install http.cache,http.cors,http.expires,http.filter,http.git,tls.dns.cloudflare;;
+ 3) bash /tmp/caddy start;;
+ 4) bash /tmp/caddy stop;;
+ 5) bash /tmp/caddy status;;
+ 6) bash /tmp/caddy backup;;
+ 7) bash /tmp/caddy restore;;
+ 8) bash /tmp/caddy delete;;
+ 9) installphp;;
+ 10) installsql;;
+ 11) installwp;;
+ 12) about;;
+ *) echo "Sorry, Wrong number. try again, please!";;
+   esac
+
+ done
+exit;
